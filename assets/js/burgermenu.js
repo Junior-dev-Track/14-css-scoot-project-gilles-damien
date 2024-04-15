8bd98c43e4fb441d8c686ee665169a757b3b8c01
ImadeusF <damien.fournier1981@gmail.com>
2024-04-15 14:03:51 +0200

    Finalisation

--- assets/js/burgermenu.js
+++ assets/js/burgermenu.js
@@ -1,11 +1,13 @@
 function openMenuMobile() {
     document.querySelector(".header__nav").classList.add("open");
     document.querySelector(".overlay-menu-mobile").classList.add("open");
+    document.querySelector(".header__logo").classList.add("logo-open");
     document.querySelector(".header__burger").classList.add("hide");
 }
 function closeMenuMobile() {
     document.querySelector(".header__nav").classList.remove("open");
     document.querySelector(".overlay-menu-mobile").classList.remove("open");
+    document.querySelector(".header__logo").classList.remove("logo-open");
     document.querySelector(".header__burger").classList.remove("hide");
 }
 
